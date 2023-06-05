2a6f41e036c8d6b84a9b8003e57e42821f874d1f
Jonibek Sayfullaev <jony@Jonibeks-MacBook-Air.local>
2023-06-05 21:19:39 +0900

    feat: develop product controller logic

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -82,6 +82,18 @@ restaurantController.logout = (req, res) => {
   res.send("Logout Page");
 };
 
+restaurantController.validateAuthRestaurant = (req, res, next) => {
+  if (req.session?.member?.mb_type === "RESTAURANT") {
+    req.member = req.session.member;
+    next();
+  } else {
+    res.json({
+      state: "fail",
+      message: "only authenticated members with restaurant type",
+    });
+  }
+};
+
 restaurantController.checkSessions = (req, res) => {
   if (req.session?.member) {
     res.json({ state: "success", data: req.session.member });
--- router_bssr.js
+++ router_bssr.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router_bssr = express.Router();
 const restaurantController = require("./controllers/restaurantController");
 const { router } = require("./app");
+const productController = require("./controllers/productController");
 
 /**************************
  *        REST API        *
  * ************************/
 
-// member
+// member route
 router_bssr
   .get("/signup", restaurantController.getSignUpMyRestaurant)
   .post("/signup", restaurantController.signupProcess);
@@ -17,8 +18,13 @@ router_bssr
   .post("/login", restaurantController.loginProcess);
 
 router_bssr.get("/logout", restaurantController.logout);
-
-router_bssr.get("/products/menu", restaurantController.getMyRestaurantData);
-
 router_bssr.get("/check-me", restaurantController.checkSessions);
+// product route
+router_bssr.get("/products/menu", restaurantController.getMyRestaurantData);
+router_bssr.post(
+  "/products/create",
+  restaurantController.validateAuthRestaurant,
+  productController.addNewProduct
+);
+router_bssr.post("/products/edit/:id", productController.updateChosenProduct);
 module.exports = router_bssr;
